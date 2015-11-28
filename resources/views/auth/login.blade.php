b3519b808d4e12d5b7f2f0fe6248eb1da963fbbe
Travis Britz <yawhatnever@gmail.com>
2015-11-28 17:52:30 -0600

    Removed margin from top

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,13 +6,13 @@
 
 <style>
     body {
-        padding-top: 40px;
+        //padding-top: 40px;
         padding-bottom: 40px;
         background-color: #eee;
     }
 
     .form-signin {
-        max-width: 330px;
+        max-width: 400px;
         padding: 15px;
         margin: 0 auto;
     }
@@ -60,23 +60,26 @@
     </div>
     @endif
 
-    <form class="form-signin" method="POST" action="/auth/login">
-        {!! csrf_field() !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <form class="form-signin" method="POST" action="/auth/login">
+                {!! csrf_field() !!}
 
-        <h2 class="form-signin-heading">Sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email" value="{{ old('email') }}">
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="remember-me" name="remember"> Remember me
-            </label>
+                <h2 class="form-signin-heading">Sign in</h2>
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email" value="{{ old('email') }}">
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me" name="remember"> Remember me
+                    </label>
+                </div>
+
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
         </div>
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-    </form>
-
+    </div>
 
 </div> <!-- /container -->
 
